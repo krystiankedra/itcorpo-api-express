085e08ff7a287d1c2bef6977e3a98dbe47cf9f54
Baca <pawel.baca@atos.net>
2020-04-15 12:58:12 +0200

    server.js

--- svc/public/server.js
+++ svc/public/server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const PORT = 3010
 
-const { getMergedDataEmployeesToProject } = require('./api/index')
+const { getMergedDataEmployeesToProject, getBenefits } = require('./api/index')
 
 app.get('/projects/:id', async (req, res, next) => {
   try {
@@ -14,6 +14,16 @@ app.get('/projects/:id', async (req, res, next) => {
     res.status(400).send(error)
   }
 })
+
+app.get('/benefits', async (req, res, next) => {
+  try {
+    const benefits = await getBenefits()
+    res.send(benefits)
+    next()
+  } catch (error) {
+    res.status(400).send(error)
+  }
+}) 
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`)
